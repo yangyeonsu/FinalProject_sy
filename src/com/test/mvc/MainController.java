497a4b40b06459c5e5d6244215f0e5c8ce4b12ad
Seoyeong <tjdud5679@naver.com>
2023-08-01 17:16:25 +0900

    [file add] xml/controller/dao/dto추가

---
+++ src/com/test/mvc/MainController.java
@@ -0,0 +1,41 @@
+package com.test.mvc;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value="/main.action", method=RequestMethod.GET)
+	public String storeList(Model model)
+	{
+		String result = "";
+		
+		IMainDAO dao = sqlSession.getMapper(IMainDAO.class);
+		
+		
+		List<Integer> ibmat_list = dao.getIbmatStNumber(); 
+		List<Integer> jjim_list = dao.getJjimStNumber(); 
+		List<Integer> hot_list = dao.getHotStNumber();
+	 
+		
+		model.addAttribute("ibmat_list", dao.getStoreList(ibmat_list));
+		model.addAttribute("jjim_list", dao.getStoreList(jjim_list));
+		model.addAttribute("hot_list", dao.getStoreList(hot_list));
+		 
+		
+		result = "MY_personal_main.jsp";
+		
+		return result;
+	}
+
+}
